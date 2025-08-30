6187e6a6024d3d5328b1e45c533e898efc677db5
TTF-fog <74908128+TTF-fog@users.noreply.github.com>
2025-08-30 20:46:07 +0530

    Update Blank Project from VGA Playground

--- project.v
+++ project.v
@@ -110,6 +110,83 @@ localparam [7:0] N[0:7] = '{
     8'b11000011   
 };
 
+localparam [7:0] E[0:7] = '{
+    8'b11111111,  
+    8'b11111111,  
+    8'b10000000,  
+    8'b11111111,  
+    8'b11111111,  
+    8'b10000000, 
+    8'b11111111, 
+    8'b11111111 
+};
+
+localparam [7:0] C[0:7] = '{
+    8'b01111110,  
+    8'b11000011,  
+    8'b10000000,  
+    8'b10000000,  
+    8'b10000000,  
+    8'b10000000, 
+    8'b11000011, 
+    8'b01111110   
+};
+
+localparam [7:0] I[0:7] = '{
+    8'b11111111,  
+    8'b00011000,  
+    8'b00011000,  
+    8'b00011000,  
+    8'b00011000,  
+    8'b00011000, 
+    8'b00011000, 
+    8'b11111111   
+};
+
+localparam [7:0] L[0:7] = '{
+    8'b10000000,
+    8'b10000000,
+    8'b10000000,
+    8'b10000000,
+    8'b10000000,
+    8'b10000000,
+    8'b10000000,
+    8'b11111111
+};
+
+localparam [7:0] W[0:7] = '{
+    8'b11000011,  
+    8'b11000011,  
+    8'b11000011,  
+    8'b11000011,  
+    8'b11011011,  
+    8'b11011011, 
+    8'b11111111, 
+    8'b01100110   
+};
+
+localparam [7:0] S[0:7] = '{
+    8'b01111110,  
+    8'b11000011,  
+    8'b11000000,  
+    8'b01111110,  
+    8'b00000011,  
+    8'b00000011, 
+    8'b11000011, 
+    8'b01111110   
+};
+
+localparam [7:0] Exclamation[0:7] = '{
+    8'b00011000,  
+    8'b00011000,  
+    8'b00011000,  
+    8'b00011000,  
+    8'b00011000,  
+    8'b00000000, 
+    8'b00011000, 
+    8'b00011000   
+};
+
 localparam [7:0] Colon[0:7] = '{
     8'b00000000,  
     8'b00011000,  
@@ -154,8 +231,8 @@ localparam [7:0] Colon[0:7] = '{
       8'b11111111
   };
   
-  reg  [8:0] state = 9'b000000000;
-  reg  [8:0] placed = 9'b000000000;
+  reg  [8:0] state = 9'b0000000111;
+  reg  [8:0] placed = 9'b000000111;
   reg [8:0] selected = 9'b00000001; 
   wire [8:0] sel_active;
   wire [11:0] btn_in = {
@@ -165,11 +242,12 @@ localparam [7:0] Colon[0:7] = '{
 };
 reg [11:0] last_btn;
 wire [11:0] btn_pressed;
+wire [1:0] win;
  genvar c;
 generate
   for (c = 0; c < 11; c = c+1) begin : BUTTON_POPULATE
     assign btn_pressed[c] = btn_in[c] & ~last_btn[c];
-  end
+  end//
 endgenerate
 
   reg turn = 1'b0;
@@ -178,10 +256,46 @@ endgenerate
   assign turn_t = glyph_active(30, 455, T, 3);
   assign turn_u = glyph_active(60, 455, U, 3);
   assign turn_r = glyph_active(90, 455, R_letter, 3);
+
+
+parameter circle_win_x = 160;
+parameter circle_win_y = 220;
+parameter text_size = 6;
+wire circle_wins_c = glyph_active(circle_win_x + 0*30, circle_win_y, C, 3);
+wire circle_wins_i1 = glyph_active(circle_win_x + 1*30, circle_win_y, I, 3);
+wire circle_wins_r = glyph_active(circle_win_x + 2*30, circle_win_y, R_letter, 3);
+wire circle_wins_c2 = glyph_active(circle_win_x + 3*30, circle_win_y, C, 3);
+wire circle_wins_l = glyph_active(circle_win_x + 4*30, circle_win_y, L, 3);
+wire circle_wins_e = glyph_active(circle_win_x + 5*30, circle_win_y, E, 3);
+wire circle_wins_w = glyph_active(circle_win_x + 7*30, circle_win_y, W, 3);
+wire circle_wins_i2 = glyph_active(circle_win_x + 8*30, circle_win_y, I, 3);
+wire circle_wins_n = glyph_active(circle_win_x + 9*30, circle_win_y, N, 3);
+wire circle_wins_s = glyph_active(circle_win_x + 10*30, circle_win_y, S, 3);
+wire circle_wins_excl = glyph_active(circle_win_x + 11*30, circle_win_y, Exclamation, 3);
+
+wire [10:0] Circle_spelling = {circle_wins_excl, circle_wins_s, circle_wins_n, circle_wins_i2, circle_wins_w, 
+                              circle_wins_e, circle_wins_l, circle_wins_c2, circle_wins_r, circle_wins_i1, circle_wins_c};
+
+
+parameter cross_win_x = 200;
+parameter cross_win_y = 2000;
+wire cross_wins_c = glyph_active(cross_win_x + 0*30, cross_win_y, C, 3);
+wire cross_wins_r = glyph_active(cross_win_x + 1*30, cross_win_y, R_letter, 3);
+wire cross_wins_o = glyph_active(cross_win_x + 2*30, cross_win_y, Circle, 3);  // Using
+wire cross_wins_s1 = glyph_active(cross_win_x + 3*30, cross_win_y, S, 3);
+wire cross_wins_s2 = glyph_active(cross_win_x + 4*30, cross_win_y, S, 3);
+wire cross_wins_w = glyph_active(cross_win_x + 6*30, cross_win_y, W, 3);
+wire cross_wins_i = glyph_active(cross_win_x + 7*30, cross_win_y, I, 3);
+wire cross_wins_n = glyph_active(cross_win_x + 8*30, cross_win_y, N, 3);
+wire cross_wins_s3 = glyph_active(cross_win_x + 9*30, cross_win_y, S, 3);
+wire cross_wins_excl = glyph_active(cross_win_x + 10*30, cross_win_y, Exclamation, 3);
+
+wire [10:0] Cross_spelling = {cross_wins_excl, cross_wins_s3, cross_wins_n, cross_wins_i, cross_wins_w,
+                             cross_wins_s2, cross_wins_s1, cross_wins_o, cross_wins_r, cross_wins_c};
+
   assign turn_n = glyph_active(120, 455, N, 3);
   assign turn_colon = glyph_active(150, 455, Colon, 3);
   assign current_item = glyph_active(180, 455, turn ? Circle : Cross, 3);
-  
   wire [8:0] cell_active;
   localparam CELL_SIZE = 80;   
   localparam CELL_SPACING = 250; 
@@ -219,7 +333,7 @@ genvar a;
     assign sel_active[a] = glyph_on;  
   end
 endgenerate
-
+assign win = check_for_win(placed,state);
 always @(posedge clk) begin
   R <= 2'b00;
   G <= 2'b00;
@@ -251,28 +365,59 @@ always @(posedge clk) begin
     3 = A
     11 = Unknown
     10 = Y
+    9 = Select
+    8 = Start
+    6 = Down Arrow
+    5 = Left Arrow
+    2 = X
     */
     //scroll around the grid
-    if (btn_pressed[9]) begin //left arrow
+    if (btn_pressed[5]) begin //left arrow
       selected <= {selected[0], selected[8:1]};
+    end 
+    if (btn_pressed[4]) begin
+        selected <= {selected[7:0], selected[8]};
     end
-    
+    if (btn_pressed[6]) begin
+      selected <= { selected[5:0], selected[8:6] };
+    end
+    if (btn_pressed[7]) begin
+      selected <= { selected[2:0], selected[8:3] };
+    end
+    if (btn_pressed[0]) begin 
+      state <= {9{1'b0}};
+      placed <= {9{1'b0}};
+    end
      if (|sel_active) begin
         G <= 2'b11;
         end
-        
+      
+      
      if (turn_t || turn_u || turn_r || turn_n || turn_colon || current_item) begin
       {R, G, B} <= 6'b111111;
     end
 
-      if (btn_pressed[3]) begin //A
+      if (btn_pressed[9]) begin //Select
         if (!state[sel_index]) begin
           state[sel_index] <= 1'b1;
           placed[sel_index] <= turn ? 1'b1 : 1'b0;
           turn <= !turn;
         end
       end
-    
+    if (win[0]) begin
+    if (win[1]) begin
+      {R, G, B} <= 6'b0000000;
+      if (|Circle_spelling) begin
+        {R, G, B} <= 6'b111111;
+      end
+      
+    end else begin
+    {R, G, B} <= 6'b0000000;
+        if (|Cross_spelling) begin
+        {R, G, B} <= 6'b111111;
+      end
+    end
+   end
 last_btn <= btn_in;
   end
 end
@@ -289,7 +434,23 @@ function [3:0] high_index;
     end
     end
     endfunction
-    
+function [1:0] check_for_win;
+    input [8:0] placed;
+    input [8:0] state;
+    begin
+       reg win_top_horizontal = 1'b0;
+       reg sign = 1'b0;
+      
+      
+      if (state[0] && state[1] && state[2]) begin
+        win_top_horizontal = (placed[0] == placed[1]) && (placed[0] == placed[2]);
+        sign = win_top_horizontal ? placed[0]: 1'b0;
+      end
+
+      check_for_win = {sign,win_top_horizontal};
+    end
+    endfunction
+
 function glyph_active;
     input [9:0] x0, y0;
     input [7:0] glyph[0:7];
